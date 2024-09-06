0e503bb80d7ab65bd3b8c6ec1ed2cd2f724c851e
greciagarciav <garciav.grecia@gmail.com>
2024-09-06 16:49:35 -0500

    Make progress of footer

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -5,24 +5,35 @@ export function Footer() {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200 dark:bg-slate-800">
       <Container>
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Statically Generated with Next.js.
-          </h3>
+        <div className="py-20 flex flex-col lg:flex-row items-center">
+          <div className="flex items-center mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
+            <img src="https://yt3.googleusercontent.com/ytc/AIdro_mBMy56YKY8Wseh8dQGpDUw3OzkDPTusPyKptW5JBkAuQ=s160-c-k-c0x00ffffff-no-rj" className="w-12 h-12 rounded-full" alt="Logo de Chica Suculentas" />
+            <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pl-2">
+              Chica Suculentas
+            </h1>
+          </div>
           <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
             <a
               href="https://nextjs.org/docs/app/building-your-application/routing/layouts-and-templates"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
-              Read Documentation
+              CANAL DE YOUTUBE
             </a>
             <a
               href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
               className="mx-3 font-bold hover:underline"
             >
-              View on GitHub
+              INSTAGRAM
             </a>
           </div>
+        </div>
+        <div className="py-10 flex flex-col items-center">
+          <h3 className="text-2xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0">
+          © 2024 Chica Suculentas - Plantas, suculentas y más. Todos los derechos reservados.
+          </h3>
+          <h3 className="py-2 text-2xl font-bold tracking-tighter leading-tight text-center mb-10 lg:mb-0">
+          Diseño y desarrollo web por greciagarciav
+          </h3>
         </div>
       </Container>
     </footer>
