b222d413714cb634aae1243a81f039f032568dd6
greciagarciav <garciav.grecia@gmail.com>
2024-09-06 16:10:49 -0500

    Add initial changes

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -1,20 +1,26 @@
-import { CMS_NAME } from "@/lib/constants";
-
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
-      </h1>
+    <section className="flex-col md:flex-row flex items-center md:justify-between mt-10 mb-16 md:mb-12">
+      <div className="flex items-center">
+        <img src="https://yt3.googleusercontent.com/ytc/AIdro_mBMy56YKY8Wseh8dQGpDUw3OzkDPTusPyKptW5JBkAuQ=s160-c-k-c0x00ffffff-no-rj" className="w-12 h-12 rounded-full" alt="Logo de Chica Suculentas" />
+        <h1 className="text-5xl font-bold tracking-tighter leading-tight md:pl-2">
+          Chica Suculentas
+        </h1>
+      </div>      
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{" "}
+        Plantas, Suculentas y Más en{" "}
         <a
-          href="https://nextjs.org/"
+          href="https://www.youtube.com/channel/UCxswVwJWrMP5cOANwH9KGDQ"
           className="underline hover:text-blue-600 duration-200 transition-colors"
         >
-          Next.js
-        </a>{" "}
-        and {CMS_NAME}.
+          Youtube
+        </a> e{" "}
+        <a
+          href="https://www.instagram.com/chicasuculentas/"
+          className="underline hover:text-blue-600 duration-200 transition-colors"
+        >
+          Instagram.
+        </a>
       </h4>
     </section>
   );
