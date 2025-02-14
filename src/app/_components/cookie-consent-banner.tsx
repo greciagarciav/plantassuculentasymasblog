07bb55b471d48a39448044b07a510362f219f379
greciagarciav <51206642+greciagarciav@users.noreply.github.com>
2025-02-14 10:15:03 -0500

    Create cookie-consent-banner component

---
+++ src/app/_components/cookie-consent-banner.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import CookieConsent from "react-cookie-consent";
+import Link from "next/link";
+
+const CookieConsentBanner = () => {
+  return (
+    <CookieConsent
+      location="bottom"
+      buttonText="Accept All"
+      declineButtonText="Decline"
+      enableDeclineButton
+      cookieName="yourAppCookieConsent"
+      style={{ background: "#2B373B", color: "#FFF" }}
+      buttonStyle={{ backgroundColor: "#4CAF50", color: "#FFF", fontSize: "14px" }}
+      declineButtonStyle={{ backgroundColor: "#f44336", color: "#FFF", fontSize: "14px" }}
+      expires={365}  // Number of days before the cookie expires
+      onAccept={() => {
+        // Add functionality when user accepts cookies
+        console.log("Cookies accepted");
+      }}
+      onDecline={() => {
+        // Add functionality when user declines cookies
+        console.log("Cookies declined");
+      }}
+    >
+      Esta página web usa cookies para mejorar tu experiencia. Al usar esta página web, das tu consentimiento para usar cookies. 
+      Más detalles sobre en <Link href="/politica-de-privacidad"><a>politica de privacidad</a></Link>.
+    </CookieConsent>
+  );
+};
+
+export default CookieConsentBanner;
